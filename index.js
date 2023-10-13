e4835f30e678faf9bb27773298552b2acd31662e
Ronney Arias <ronneyarias@gmail.com>
2023-10-12 21:20:15 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function saturdayFun(string = 'roller-skate'){
+    return `This Saturday, I want to ${string}!`;
+}
+
+
+function mondayWork(string = 'go to the office'){
+    return `This Monday, I will ${string}.`;
+   }
+   console.log(mondayWork());
+
+function wrapAdjective(flair = "*") {
+    return function (adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`
+    }
+  }
